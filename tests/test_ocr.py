ef50b868316eae68835dfac10d48cda91d634b3c
John <jasszelag2006@gmail.com>
2023-08-27 17:42:28 +0200

    update tests

--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from ocr import OCR
 
 images = ["brel_g1.png", "brel_g2.png", "valtan_g1_1.png", "valtan_g1_2.png", "valtan_g1_3.png", "valtan_g2_1.png", "valtan_g2_2.png", "clown_g1.png", "clown_g2.png", "clown_g3.png", "clown_bingo.png"]
-images = ["tests\examples/" + image for image in images]
+images = ["tests/examples/" + image for image in images]
 
 def test_ocr_calibrate():
     for image in images:

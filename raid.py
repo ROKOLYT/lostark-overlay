a141aa296f4d547c112b69b4a8159202c83d0c57
John <jasszelag2006@gmail.com>
2023-08-30 15:43:04 +0200

    add brel gates 3-6

--- raid.py
+++ raid.py
@@ -51,8 +51,43 @@ class Raids:
         brel_g2 = Gate([
             Mechanic("Just kill", 1)],
                        ["Prokel's Spiritual Echo"])
-
-        self.brel = Boss("brel", [brel_g1, brel_g2])
+        brel_g3 = Gate([
+            Mechanic("x145 Meteor and stagger", 145),
+            Mechanic("x100 Medusa stagger", 100),
+            Mechanic("x42 Shapes", 42),
+            Mechanic("x0 Scythes and spears", 0)],
+                       ["Ashtarot"])
+        brel_g4 = Gate([
+            Mechanic("x170 Wipe mech", 170),
+            Mechanic("x160 Color change", 160),
+            Mechanic("x135 Color change", 135),
+            Mechanic("x120 Wipe mech", 120),
+            Mechanic("x95 Stagger", 95),
+            Mechanic("x65 Color change", 65),
+            Mechanic("x55 Wipe mech", 55),
+            Mechanic("x40 Color change", 40),
+            Mechanic("x20 Stagger", 20)],
+                       ["Primordial Nightmare"])
+        brel_g5 = Gate([
+            Mechanic("x180 Shapes appear", 180),
+            Mechanic("x144 Last shapes", 144),
+            Mechanic("x140 Spots x3", 140),
+            Mechanic("x110 memory stagger - cubes - stagger / safe spot", 110),
+            Mechanic("x90 Shapes appear", 90),
+            Mechanic("x54 Last shapes", 54),
+            Mechanic("x50 Spots x3 + 1", 50)],
+                       ["Brelshaza. Monarch of Nightmares"])
+        brel_g6 = Gate([
+            Mechanic("x222 Tornado", 222),
+            Mechanic("x212 Shape dimension", 212),
+            Mechanic("x188 Meteors", 188),
+            Mechanic("x112 Worship", 112),
+            Mechanic("x65 Tornado", 65),
+            Mechanic("x32 Shape Dimension", 32),
+            Mechanic("x25 Corner breaking", 25)],
+                       ["Phantom Legion Commander Brelshaza"])
+            
+        self.brel = Boss("brel", [brel_g1, brel_g2, brel_g3, brel_g4, brel_g5, brel_g6])
 
         clown_g1 = Gate([
             Mechanic("x130 Reflect stagger", 130),
